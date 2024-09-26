95f2c53672ff1f0ec9596f1b8705e70e4c54bcac
Lutzoww <lutzoww51@gmail.com>
2024-09-26 21:01:06 +0500

    second commit

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,33 +1,78 @@
-#include <iostream>
+﻿#include <iostream>
 
 int main()
 {
 
-    int number;
-    int total_sum = 0;
-    int even_sum = 0;
-    int odd_sum = 0;
+	double  count_of_rubles, count_of_money, commission,
+	commission_rate = 0.05,
+	dollars = 92.86,
+	euro = 103.41,
+	yuan = 13.21,
+	farit = 37.00,
+	yen = 0.64;
+	int currency;
+	char restart;
 
-    std::cout << "Enter numbers (0 to exit): \n";
+	do
+	{
+		std::cout << "Enter how many rubles you want to exchange: ";
+		std::cin >> count_of_rubles;
 
-    while (true) {
-        std::cin >> number;
-        if (number == 0) {
-            break;
-        }
-        total_sum += number;
+		std::cout << "Currencies available for exchange:\n1. Dollars\t\t" << dollars
+			<< "\n" << "2. Euro\t\t\t" << euro << "\n3. Yuan\t\t\t" << yuan
+			<< "\n4. Farit \t\t" << farit << "\n5. Yen\t\t\t" << yen << "\n";
 
-        if (number % 2 == 0) {
-            even_sum += number;
-        }
-        else {
-            odd_sum += number;
-        }
-    }
+		std::cout << "Enter the currency number for exchange: ";
+		std::cin >> currency;
 
-    std::cout << "Sum of all entered numbers: " << total_sum << "\n";
-    std::cout << "Sum of even numbers: " << even_sum << "\n";
-    std::cout << "Sum of odd numbers: " << odd_sum << "\n";
+		if (currency == 1) // dollars
+		{
+			count_of_money = count_of_rubles / dollars;
+			commission = count_of_rubles * commission_rate;
+			std::cout << "You will be able to get: " << count_of_money << " Dollars\n"
+				<< "Transfer fee: " << commission << "\n"
+				<< "Total: " << count_of_rubles + commission;
+		}
+		else if (currency == 2) // euro
+		{
+			count_of_money = count_of_rubles / euro;
+			commission = count_of_rubles * commission_rate;
+			std::cout << "You will be able to get: " << count_of_money << " Euro\n"
+				<< "Transfer fee: " << commission << "\n"
+				<< "Total: " << count_of_rubles + commission;
+		}
+		else if (currency == 3) // yuan
+		{
+			count_of_money = count_of_rubles / yuan;
+			commission = count_of_rubles * commission_rate;
+			std::cout << "You will be able to get: " << count_of_money << " Yuan\n"
+				<< "Transfer fee: " << commission << "\n"
+				<< "Total: " << count_of_rubles + commission;
+		}
+		else if (currency == 4) // farit
+		{
+			count_of_money = count_of_rubles / farit;
+			commission = count_of_rubles * commission_rate;
+			std::cout << "You will be able to get: " << count_of_money << " Farits\n"
+				<< "Transfer fee: " << commission << "\n"
+				<< "Total: " << count_of_rubles + commission;
+		}
+		else if (currency == 5) // yen
+		{
+			count_of_money = count_of_rubles / yen;
+			commission = count_of_rubles * commission_rate;
+			std::cout << "You will be able to get: " << count_of_money << " Yen\n"
+				<< "Transfer fee: " << commission << "\n"
+				<< "Total: " << count_of_rubles + commission << "";
+		}
+		else
+		{
+			std::cout << "Currency input error.\n";
+		}
 
+		std::cout << "\nRepeat the exchange?\n";
+		std::cin >> restart;
+
+	} while (restart == 'y' || restart == 'Y');
 	return 0;
 }
